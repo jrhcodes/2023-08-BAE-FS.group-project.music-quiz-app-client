267343beaec0ca928e7520e610b98c87a3ab9fe9
Julian <mrjrhirst@gmail.com>
2023-11-05 00:05:46 +0000

    CHanged Header to The A-Listers' Swap Sounds

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
-const Header: React.FC = () => <><h1>🎵🎶The A-Listers: Swap Sounds🎵🎶</h1></>;
+const Header: React.FC = () => <><h1>🎵🎶The A-Listers' Swap Sounds🎵🎶</h1></>;
 export default Header;
